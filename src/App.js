e1e5ec32b56fa528371defca0f099c3cbb688eec
JuliusMFMota <jmaisonnetteferreiramota@gmail.com>
2024-01-02 21:32:26 -0300

    a

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
         
         <div className="cabeçalho">
          <h1 className="title">Front-End Developer</h1>
-         <h2 className="info">Bem vindo!</h2>
+         <h2 className="info">Bem vindo! oi</h2>
          <h2 className="apresentação">Este é o projeto React feito pelo desenvolvedor Julius Maisonnette Ferreira Mota.<br />Nesta pagina terão <em>tutorias de matematica e de formulas simples</em> <br />acompanhado de calculadoras para resolver essas formulas.</h2>
         </div>
       </div>
